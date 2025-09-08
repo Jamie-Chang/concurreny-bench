a63c0252592c1ae468c3023733ef58f9f98bb85c
Jamie Chang <jamie.cheng.chang@gmail.com>
2025-09-08 12:24:47 +0100

    Add examples of different types of backpressure using semaphores

---
+++ backpressure.py
@@ -0,0 +1,31 @@
+import asyncio
+from typing import Iterator
+import random
+import tracemalloc
+from itertools import islice
+
+
+def urls() -> Iterator[str]:
+    while True:
+        yield "https://example.com"
+
+
+async def process_url(url: str, semaphore: asyncio.Semaphore) -> None:
+    async with semaphore:
+        await asyncio.sleep(random.uniform(0.1, 0.2))
+
+
+async def main() -> None:
+    tracemalloc.start()
+    semaphore = asyncio.Semaphore(500)
+    async with asyncio.TaskGroup() as tg:
+        for url in islice(urls(), 100_000):
+            tg.create_task(process_url(url, semaphore))
+
+    _, peak_memory = tracemalloc.get_traced_memory()
+    print(f"Peak memory usage: {peak_memory:_} bytes")
+    tracemalloc.stop()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
